9759fff1ed2962eb41c20e4d41646bd06d04ab4d
yunan9 <itzcollor@gmail.com>
2025-11-25 16:57:55 -0500

    feat: start builder for del expression config

--- src/main/java/io/github/yunan9/dpex/configuration/DelimitedExpressionConfiguration.java
+++ src/main/java/io/github/yunan9/dpex/configuration/DelimitedExpressionConfiguration.java
@@ -1,4 +1,24 @@
 package io.github.yunan9.dpex.configuration;
 
+import static io.github.yunan9.dpex.configuration.DelimitedExpressionConfigurationImpl.*;
+
+import io.github.yunan9.commons.build.Buildable;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 public sealed interface DelimitedExpressionConfiguration
-    permits DelimitedExpressionConfigurationImpl {}
+    permits DelimitedExpressionConfigurationImpl {
+
+  @Contract(value = " -> new", pure = true)
+  static @NotNull Builder newBuilder() {
+    return new BuilderImpl();
+  }
+
+  sealed interface Builder extends Buildable<@NotNull DelimitedExpressionConfiguration>
+      permits BuilderImpl {
+
+    @Override
+    @NotNull
+    DelimitedExpressionConfiguration build();
+  }
+}
--- src/main/java/io/github/yunan9/dpex/configuration/DelimitedExpressionConfigurationImpl.java
+++ src/main/java/io/github/yunan9/dpex/configuration/DelimitedExpressionConfigurationImpl.java
@@ -1,3 +1,16 @@
 package io.github.yunan9.dpex.configuration;
 
-final class DelimitedExpressionConfigurationImpl implements DelimitedExpressionConfiguration {}
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+final class DelimitedExpressionConfigurationImpl implements DelimitedExpressionConfiguration {
+
+  @ApiStatus.Internal
+  static final class BuilderImpl implements Builder {
+
+    @Override
+    public @NotNull DelimitedExpressionConfiguration build() {
+      return null;
+    }
+  }
+}
